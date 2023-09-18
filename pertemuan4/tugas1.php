3311e2d4e2a17b933247a2a2f4fd3695f2bb668c
nrlhikma554 <nrlhikmah554@gmail.com>
2023-09-18 11:44:29 +0800

    menambahkan revisi praktikum php

--- pertemuan4/tugas1.php
+++ pertemuan4/tugas1.php
@@ -8,7 +8,7 @@
    $saldoAwal = 2000000;
    $bunga = 0.03;
    $bulan = 11;
-   $saldoAkhir = $saldoAwal * (1 + $bunga )** $bulan;
+   $saldoAkhir = $saldoAwal + ($saldoAwal * $bunga * $bulan);
    echo "Saldo akhir setelah ".$bulan." bulan adalah : Rp. ".number_format($saldoAkhir, 2).",-";
    ?>
 </body>
